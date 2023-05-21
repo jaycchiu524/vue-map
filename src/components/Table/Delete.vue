b238e32c3e2bdae1f92c0f977fc1798c98089420
Jay Chiu <jaycchiu524@gmail.com>
2023-05-21 15:49:40 -0400

    feat(ui): add select & delete btn

---
+++ src/components/Table/Delete.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-tooltip :text="text" location="end">
+    <template v-slot:activator="{ props }">
+      <v-icon
+        v-bind="props"
+        icon="mdi-delete"
+        class="cursor-pointer"
+        color="red darken-1"
+        @click="() => emit('delete')"
+      />
+    </template>
+  </v-tooltip>
+</template>
+<script setup lang="ts">
+import { Row } from "@tanstack/vue-table";
+import { computed } from "vue";
+import { LocationData } from "../types";
+
+const props = defineProps({
+  row: {
+    type: Object as () => Row<LocationData>,
+    required: true,
+  },
+});
+const emit = defineEmits(["delete"]);
+
+const text = computed(() => {
+  return `Delete ${props.row.original.name || "this location"}`;
+});
+</script>
+<style lang=""></style>
