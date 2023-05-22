96ad11b37f751a842b1b5c59a665367193604e60
Jay Chiu <jaycchiu524@gmail.com>
2023-05-22 02:08:29 -0400

    fix: remove watch

--- src/components/CurrentLocationCard.vue
+++ src/components/CurrentLocationCard.vue
@@ -1,17 +1,21 @@
 <template>
-  <v-card v-if="!!location" :title="location.name" :subtitle="location.address">
+  <v-card
+    class="mx-auto d-flex text-start flex-column"
+    max-width="500"
+    v-if="!!location"
+    :title="location.name"
+    :subtitle="location.address">
+    <v-card-text>
+      <div class="tw-text-[48px] tw-leading-tight">{{ local.time }}</div>
+      <div>{{ local.date }}</div>
+    </v-card-text>
     <v-card-text>
       <div>
-        <span class="font-weight-bold">Local Time: </span>
-        <span>
-          {{ local }}
-        </span>
+        <span class="font-weight-bold">UTC Time: </span>
+        <span>{{ utc.date }}</span>
+        <span>{{ utc.time }}</span>
       </div>
       <div>
-        <span class="font-weight-bold">UTC Time: </span>
-        <span>{{ utc }}</span>
-      </div>
-      <div v-if="!!timezone">
         <span class="font-weight-bold">Time Zone: </span>
         <span>{{ timezone }}</span>
       </div>
@@ -23,18 +27,18 @@
   </v-card>
 </template>
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted, computed, watch } from "vue";
-import { utcToZonedTime, format } from "date-fns-tz";
-import axios from "axios";
-import { LocationData } from "./types";
-import { PropType } from "vue";
+import { ref, onMounted, onUnmounted, computed, watch } from 'vue'
+import { utcToZonedTime, format } from 'date-fns-tz'
+import axios from 'axios'
+import { LocationData } from './types'
+import { PropType } from 'vue'
 
 const getTimezone = async (lat: number, lng: number) => {
-  const API_KEY = import.meta.env.VITE_GOOGLE_MAP_API_KEY;
+  const API_KEY = import.meta.env.VITE_GOOGLE_MAP_API_KEY
 
-  if (!API_KEY) throw new Error("API_KEY is not defined");
+  if (!API_KEY) throw new Error('API_KEY is not defined')
 
-  const timestamp = Math.floor(Date.now() / 1000);
+  const timestamp = Math.floor(Date.now() / 1000)
   const response = await axios.get(
     `https://maps.googleapis.com/maps/api/timezone/json`,
     {
@@ -43,49 +47,56 @@ const getTimezone = async (lat: number, lng: number) => {
         timestamp,
         key: API_KEY,
       },
-    }
-  );
-
-  return response.data;
-};
+    },
+  )
+  return response.data
+}
 
 const props = defineProps({
   location: {
     type: Object as PropType<LocationData>,
     required: true,
   },
-});
+})
 
-const utc = ref(getUTCTime());
-const local = ref(getLocalTime());
-const timezone = ref("");
-const location = computed(() => props.location);
-
-watch(location, async (state) => {
-  const tz = await getTimezone(state?.lat!, state?.lng!);
-  timezone.value = tz.timeZoneId;
-});
+const utc = ref(getUTCTime())
+const local = ref(getLocalTime())
+const timezone = ref('')
 
 function getUTCTime() {
-  const date = new Date();
-  return format(utcToZonedTime(date, "UTC"), "yyyy-MM-dd HH:mm:ss");
+  const date = new Date()
+  return {
+    date: format(utcToZonedTime(date, 'UTC'), 'yyyy-MM-dd'),
+    time: format(utcToZonedTime(date, 'UTC'), 'HH:mm:ss'),
+  }
 }
-function getLocalTime(timezone = "America/Toronto") {
-  const date = new Date();
-  return format(utcToZonedTime(date, timezone), "yyyy-MM-dd HH:mm:ss");
+function getLocalTime(timezone = 'America/Toronto') {
+  const date = new Date()
+  return {
+    date: format(utcToZonedTime(date, timezone), 'yyyy-MM-dd'),
+    time: format(utcToZonedTime(date, timezone), 'HH:mm:ss'),
+  }
 }
 
-let intervalId: NodeJS.Timer;
+const updateTimezone = async () => {
+  if (!props.location) return
+  const { lat, lng } = props.location
+  const { timeZoneId } = await getTimezone(lat!, lng!)
+  timezone.value = timeZoneId
+}
 
-onMounted(async () => {
+let intervalId: NodeJS.Timer
+
+onMounted(() => {
+  updateTimezone()
   intervalId = setInterval(() => {
-    utc.value = getUTCTime();
-    local.value = getLocalTime(timezone.value);
-  }, 1000);
-});
+    utc.value = getUTCTime()
+    local.value = getLocalTime(timezone.value)
+  }, 1000)
+})
 
 onUnmounted(() => {
-  clearInterval(intervalId);
-});
+  clearInterval(intervalId)
+})
 </script>
 <style lang=""></style>
